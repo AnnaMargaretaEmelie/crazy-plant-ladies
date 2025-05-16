d69df6309c3f3191f10e31349c496e680282f86a
Anna <anna.viklund1@medieinstitutet.se>
2025-05-16 14:06:54 +0200

    Added form to add plants

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { PlantList } from "./components/PlantList";
 import "./App.css";
 import type { Plant } from "./models/Plant";
+import { PlantForm } from "./components/PlantForm";
 
 function App() {
   const [plants, setPlants] = useState<Plant[]>([
@@ -14,10 +15,15 @@ function App() {
     },
     { id: 2, name: "Habanero", category: "Chili", isSown: true, status: "Ok" },
   ]);
+
+  const addPlant = (plant: Plant) => {
+    setPlants((prev) => [...prev, plant]);
+  };
   return (
     <>
       <main>
         <h1>Crazy Plant Ladies Society</h1>
+        <PlantForm addPlant={addPlant}></PlantForm>
         <PlantList plants={plants} />
       </main>
     </>
---
+++ src/components/PlantForm.tsx
@@ -0,0 +1,61 @@
+import type { Plant } from "../models/Plant";
+import { useState } from "react";
+
+type FormProps = {
+  addPlant: (newPlant: Plant) => void;
+};
+
+export function PlantForm({ addPlant }: FormProps) {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!name.trim() || !category.trim()) return;
+
+    const newPlant: Plant = {
+      id: Date.now(),
+      name,
+      category,
+      isSown: false,
+      status,
+    };
+
+    addPlant(newPlant);
+
+    setName("");
+    setCategory("");
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h2>Lägg till ny planta</h2>
+        <div>
+          <label htmlFor="name">
+            Namn:
+            <input
+              type="text"
+              value={name}
+              id="name"
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </label>
+        </div>
+        <div>
+          <label htmlFor="category">
+            Kategori:{" "}
+            <input
+              type="text"
+              id="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">Lägg till</button>
+      </form>
+    </>
+  );
+}
